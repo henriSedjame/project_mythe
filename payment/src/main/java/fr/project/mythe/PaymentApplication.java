0042cf4d40bd4109a9de9d1827e7440fc39fbf2b
henriSedjame <sedhjo@gmail.com>
2018-10-16 21:05:45 +0200

    Ajout du main

---
+++ payment/src/main/java/fr/project/mythe/PaymentApplication.java
@@ -0,0 +1,17 @@
+package fr.project.mythe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Project project_mythe
+ * @Author Henri Joel SEDJAME
+ * @Date 16/10/2018
+ * @Class purposes : .......
+ */
+@SpringBootApplication
+public class PaymentApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentApplication.class, args);
+  }
+}
