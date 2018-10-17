4b898b8b7211b344e8bf090fcf3bf6c282240025
henri Joel SEDJAME <sedhjo@gmail.com>
2018-10-17 11:18:21 +0200

    modification du module insee_auth

---
+++ insee_authentication/src/main/java/fr/project/mythe/insee_authentication/InseeAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package fr.project.mythe.insee_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InseeAuthenticationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InseeAuthenticationApplication.class, args);
+  }
+}
